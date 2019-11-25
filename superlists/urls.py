47b595b7ca24575722b356d8f0c96389c7e17135
nexome <nex7code@gmail.com>
2019-11-25 22:45:47 +0530

    new URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -20,4 +20,5 @@ from lists import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('',views.home_page, name='home'), # django 2.0 no need for r'^$'
+    path(r'lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
 ]
